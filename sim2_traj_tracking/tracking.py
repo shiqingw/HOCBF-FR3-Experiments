eda6130597947aafe02bdbf7f7f73f7d352ff239
Shiqing Wei <shiqing.wei@outlook.com>
2024-04-30 01:57:39 -0400

    fixed desired rotation

--- sim2_traj_tracking/tracking.py
+++ sim2_traj_tracking/tracking.py
@@ -206,7 +206,7 @@ if __name__ == "__main__":
         Kd = np.diag([20,20,20,100,100,100])
         
         R_d = np.array([[1, 0, 0],
-                        [0, 1, 0],
+                        [0, -1, 0],
                         [0, 0, -1]], dtype=config.np_dtype)
         traj, traj_dt, traj_dtdt = Ttraj.get_traj_and_ders(i*dt)
         S, u_task = get_torque_to_track_traj_const_ori(traj, traj_dt, traj_dtdt, R_d, Kp, Kd, Minv_mj, J_EE, dJdq_EE, dq, P_EE, R_EE)
